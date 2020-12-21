06905f1e9f740a4e404d26f5c58965657b717463
Floyd Moore <floyd.moore@gmail.com>
2020-12-20 23:42:04 -0800

    added correct offsets for servos

--- quadbot.ino
+++ quadbot.ino
@@ -1,141 +1,439 @@
+/*
+ * Arduino program to operate quad-spider robot.
+ * The main difference between this code and the original on the instructables:
+ *  https://www.instructables.com/DIY-Spider-RobotQuad-robot-Quadruped/
+ * is caused by my use of a PCA8685 I2C Servo driver instead of direct servo control
+ */
 #include <Wire.h>
 #include <Adafruit_PWMServoDriver.h>
+#include <FlexiTimer2.h>
 
 // called this way, it uses the default address 0x40
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 
-// Depending on your servo make, the pulse width min and max may vary, you
-// want these to be as small/large as possible without hitting the hard stop
-// for max range. You'll have to tweak them as necessary to match the servos you
-// have!
-#define SERVOMIN  150 // This is the 'minimum' pulse length count (out of 4096)
-#define SERVOMAX  600 // This is the 'maximum' pulse length count (out of 4096)
-#define USMIN  600 // This is the rounded 'minimum' microsecond length based on the minimum pulse of 150
-#define USMAX  2400 // This is the rounded 'maximum' microsecond length based on the maximum pulse of 600
+// Define the limits of travel for servos connected to the PCA8685 (in usec)
+#define USMIN  500 // This is the rounded 'minimum' microsecond length based on the minimum pulse of 150
+#define USMAX  2500 // This is the rounded 'maximum' microsecond length based on the maximum pulse of 600
 #define SERVO_FREQ 50 // Analog servos run at ~50 Hz updates
+#define OSC_FREQ 27000000
 
 // our servo # counter
 uint8_t servonum = 0;
 
 //define servos' ports
-//        Coxa  Femur   Tibia
-//  Leg0: 0     1       2
-//  Leg1: 3     4       5
-//  Leg2: 6     7       8
-//  Leg3: 9     10      11
-// (Shoulder, elbow, wrist)
-// Neutral position should be out to the side and Tibia straight down.
-
+//        elbow wrist/tip shoulder
+//        femur tibia     coxa
+//  Leg0: 0     1         2
+//  Leg1: 4     5         6
+//  Leg2: 8     9         10
+//  Leg3: 12    13        14
+// Neutral position should be out to the side (all servos should be at 90deg)
 const int servo_pin[4][3] = { {0, 1, 2}, {4, 5, 6}, {8, 9, 10}, {12, 13, 14} };
-//                    leg #          1                   2                   3                   4
-//                 position    f     c     b       f     c     b       f     c     b       f     c     b
-const int coxa_pos[4][3]  = { {1000, 1500, 2000}, {2000, 1500, 1000}, {1000, 1500, 2000}, {2000, 1500, 1000} };
-const int femur_pos[4][3] = { {2000, 1500,  900}, {4, 1500, 6}, {8, 1500, 10}, {12, 1500, 14} };
-const int tibia_pos[4][3] = { {2000, 1500,  900}, {4, 1500, 6}, {8, 1500, 10}, {12, 1500, 14} };
-/*
- * leg up = f, f, f 
- */
+const int servo_offset[4][3] = { {0, 0, -80}, {0, -600, 100}, {-50, 120, 80}, {0, 150, 100} };
+
+/* Size of the robot ---------------------------------------------------------*/
+const float length_a = 55;
+const float length_b = 77.5;
+const float length_c = 27.5;
+const float length_side = 71;
+const float z_absolute = -28;
+
+/* Constants for movement ----------------------------------------------------*/
+const float z_default = -70, z_up = -30, z_boot = z_absolute;
+const float x_default = 62, x_offset = 0;
+const float y_start = 0, y_step = 40;
+const float y_default = x_default;
+
+/* variables for movement ----------------------------------------------------*/
+volatile float site_now[4][3];    //real-time coordinates of the end of each leg
+volatile float site_expect[4][3]; //expected coordinates of the end of each leg
+
+float temp_speed[4][3];   //each axis' speed, needs to be recalculated before each movement
+float move_speed;     //movement speed
+float speed_multiple = 1; //movement speed multiple
+const float spot_turn_speed = 4;
+const float leg_move_speed = 8;
+const float body_move_speed = 3;
+const float stand_seat_speed = 2;
+volatile int rest_counter;      //+1/0.02s, for automatic rest
+//functions' parameter
+const float KEEP = 255;
+//define PI for calculation
+const float pi = 3.1415926;
+
+/* Constants for turn --------------------------------------------------------*/
+//temp length
+const float temp_a = sqrt(pow(2 * x_default + length_side, 2) + pow(y_step, 2));
+const float temp_b = 2 * (y_start + y_step) + length_side;
+const float temp_c = sqrt(pow(2 * x_default + length_side, 2) + pow(2 * y_start + y_step + length_side, 2));
+const float temp_alpha = acos((pow(temp_a, 2) + pow(temp_b, 2) - pow(temp_c, 2)) / 2 / temp_a / temp_b);
+//site for turn
+const float turn_x1 = (temp_a - length_side) / 2;
+const float turn_y1 = y_start + y_step / 2;
+const float turn_x0 = turn_x1 - temp_b * cos(temp_alpha);
+const float turn_y0 = temp_b * sin(temp_alpha) - turn_y1 - length_side;
+/* ---------------------------------------------------------------------------*/
+
+int sensorValue = 0;        // value read from the pot
+int outputValue = 0;        // value output to the PWM (analog out)
 
 void setup() {
   // initialize serial communication:
   Serial.begin(9600);
-  
+
+  // set up the I2C servo mux: PCA98685
   pwm.begin();
-  pwm.setOscillatorFrequency(27000000);
+  pwm.setOscillatorFrequency(OSC_FREQ);
   pwm.setPWMFreq(SERVO_FREQ);  // Analog servos run at ~50 Hz updates
 
-}
-
-void setAngle(uint8_t servo, uint8_t angle){
-  // Angle to micro-sec
-  pwm.writeMicroseconds(servo, map(angle, 0, 180, USMIN, USMAX));
+  Serial.println("Robot starts initialization");
+  //initialize default parameter
+  set_site(0, x_default - x_offset, y_start + y_step, z_boot);
+  set_site(1, x_default - x_offset, y_start + y_step, z_boot);
+  set_site(2, x_default + x_offset, y_start, z_boot);
+  set_site(3, x_default + x_offset, y_start, z_boot);
+  for (int i = 0; i < 4; i++)
+  {
+    for (int j = 0; j < 3; j++)
+    {
+      site_now[i][j] = site_expect[i][j];
+    }
+  }
+  //start servo service
+  FlexiTimer2::set(20, servo_service);
+  FlexiTimer2::start();
+  Serial.println("Servo service started");
+  Serial.println("Robot initialization Complete");
 }
 
 void loop() {
-  int usec = 1600;
-  //int usec = 600;
-
-  // read the sensor:
-  if (Serial.available() > 0) {
-    int inByte = Serial.read();
-    // do something different depending on the character received.
-    // The switch statement expects single number values for each case; in this
-    // example, though, you're using single quotes to tell the controller to get
-    // the ASCII value for the character. For example 'a' = 97, 'b' = 98,
-    // and so forth:
-
-    switch (inByte) {
-      case '1':
-        coxaBack(0);
-        break;
-      case 'a':
-        coxaFwd(0);
-        break;
-      case 'q':
-        legUp(1, 2);
-        break;
-      case 'z':
-        legDown(1, 2);
-        break;
-      case '2':
-        coxaBack(4);
-        break;
-      case 's':
-        coxaFwd(4);
-        break;
-      case 'w':
-        legUp(5, 6);
-        break;
-      case 'x':
-        legDown(5, 6);
-        break;
-      case 'm':
-        setAllCenter();
-        break;
-      default:
-        break;
-    }
- }
-
- delay(50);
+  Serial.println("Stand");
+  stand();
+  delay(2000);
+  Serial.println("Hand wave");
+  hand_wave(3);
+  delay(2000);
+  Serial.println("Hand wave");
+  hand_shake(3);
+  delay(2000);
+  sit();
+  delay(5000);
 }
 
-void setAllCenter() {
-  for (uint16_t id = 0; id <= 14; id++){
-    pwm.writeMicroseconds(id, 1500);
-delay(50);
+void setAllCenter(void) {
+  // tibia (leg tip) position for straight out
+  pwm.writeMicroseconds(1, 2140); // tip
+  pwm.writeMicroseconds(5, 1017); // tip
+  pwm.writeMicroseconds(9, 1480); // tip
+  pwm.writeMicroseconds(13, 1705); // tip
+
+  // femur (mid leg) position for straight out
+  pwm.writeMicroseconds(0, 1500); // tip
+  pwm.writeMicroseconds(4, 1500); // tip
+  pwm.writeMicroseconds(8, 1450); // tip
+  pwm.writeMicroseconds(12, 1500); // tip
+
+  // coxa (shoulder) position for straight out
+  pwm.writeMicroseconds(2, 1424); // tip
+  pwm.writeMicroseconds(6, 1609); // tip
+  pwm.writeMicroseconds(10, 1500); // tip
+  pwm.writeMicroseconds(14, 1589); // tip
+  
+  delay(50);
+}
+
+/*
+  - sit
+  - blocking function
+   ---------------------------------------------------------------------------*/
+void sit(void)
+{
+  move_speed = stand_seat_speed;
+  for (int leg = 0; leg < 4; leg++)
+  {
+    set_site(leg, KEEP, KEEP, z_boot);
+  }
+  wait_all_reach();
+}
+
+/*
+  - stand
+  - blocking function
+   ---------------------------------------------------------------------------*/
+void stand(void)
+{
+  move_speed = stand_seat_speed;
+  for (int leg = 0; leg < 4; leg++)
+  {
+    set_site(leg, KEEP, KEEP, z_default);
+  }
+  wait_all_reach();
+}
+
+void body_left(int i)
+{
+  set_site(0, site_now[0][0] + i, KEEP, KEEP);
+  set_site(1, site_now[1][0] + i, KEEP, KEEP);
+  set_site(2, site_now[2][0] - i, KEEP, KEEP);
+  set_site(3, site_now[3][0] - i, KEEP, KEEP);
+  wait_all_reach();
+}
+
+void body_right(int i)
+{
+  set_site(0, site_now[0][0] - i, KEEP, KEEP);
+  set_site(1, site_now[1][0] - i, KEEP, KEEP);
+  set_site(2, site_now[2][0] + i, KEEP, KEEP);
+  set_site(3, site_now[3][0] + i, KEEP, KEEP);
+  wait_all_reach();
+}
+
+void hand_wave(int i)
+{
+  float x_tmp;
+  float y_tmp;
+  float z_tmp;
+  move_speed = 1;
+  if (site_now[3][1] == y_start)
+  {
+    body_right(15);
+    x_tmp = site_now[2][0];
+    y_tmp = site_now[2][1];
+    z_tmp = site_now[2][2];
+    move_speed = body_move_speed;
+    for (int j = 0; j < i; j++)
+    {
+      set_site(2, turn_x1, turn_y1, 50);
+      wait_all_reach();
+      set_site(2, turn_x0, turn_y0, 50);
+      wait_all_reach();
+    }
+    set_site(2, x_tmp, y_tmp, z_tmp);
+    wait_all_reach();
+    move_speed = 1;
+    body_left(15);
+  }
+  else
+  {
+    body_left(15);
+    x_tmp = site_now[0][0];
+    y_tmp = site_now[0][1];
+    z_tmp = site_now[0][2];
+    move_speed = body_move_speed;
+    for (int j = 0; j < i; j++)
+    {
+      set_site(0, turn_x1, turn_y1, 50);
+      wait_all_reach();
+      set_site(0, turn_x0, turn_y0, 50);
+      wait_all_reach();
+    }
+    set_site(0, x_tmp, y_tmp, z_tmp);
+    wait_all_reach();
+    move_speed = 1;
+    body_right(15);
   }
 }
 
-void coxaCenter(int id) {
-  // leg shoulder center left/right
-  pwm.writeMicroseconds(id, 1500); // shoulder
+void hand_shake(int i)
+{
+  float x_tmp;
+  float y_tmp;
+  float z_tmp;
+  move_speed = 1;
+  if (site_now[3][1] == y_start)
+  {
+    body_right(15);
+    x_tmp = site_now[2][0];
+    y_tmp = site_now[2][1];
+    z_tmp = site_now[2][2];
+    move_speed = body_move_speed;
+    for (int j = 0; j < i; j++)
+    {
+      set_site(2, x_default - 30, y_start + 2 * y_step, 55);
+      wait_all_reach();
+      set_site(2, x_default - 30, y_start + 2 * y_step, 10);
+      wait_all_reach();
+    }
+    set_site(2, x_tmp, y_tmp, z_tmp);
+    wait_all_reach();
+    move_speed = 1;
+    body_left(15);
+  }
+  else
+  {
+    body_left(15);
+    x_tmp = site_now[0][0];
+    y_tmp = site_now[0][1];
+    z_tmp = site_now[0][2];
+    move_speed = body_move_speed;
+    for (int j = 0; j < i; j++)
+    {
+      set_site(0, x_default - 30, y_start + 2 * y_step, 55);
+      wait_all_reach();
+      set_site(0, x_default - 30, y_start + 2 * y_step, 10);
+      wait_all_reach();
+    }
+    set_site(0, x_tmp, y_tmp, z_tmp);
+    wait_all_reach();
+    move_speed = 1;
+    body_right(15);
+  }
 }
 
-void coxaBack(int id) {
-  // leg back
-  pwm.writeMicroseconds(id, 2000); // shoulder
+// Emulate the default arduino servo() behavior (with an added parameter for the servo number
+//      to control):
+// Documentation from the main servo library:   
+//    Writes a value to the servo, controlling the shaft accordingly. On a standard servo,
+//    this will set the angle of the shaft (in degrees), moving the shaft to that orientation.
+//    On a continuous rotation servo, this will set the speed of the servo (with 0 being full-speed
+//    in one direction, 180 being full speed in the other, and a value near 90 being no movement).
+void servo_write(int leg, int joint, int angle) 
+{
+  int micros = map(angle, 0, 180, USMIN, USMAX) + servo_offset[leg][joint];
+  pwm.writeMicroseconds(servo_pin[leg][joint], micros);
+  Serial.print("servo ");
+  Serial.print(leg);
+  Serial.print(", ");
+  Serial.print(joint);
+  Serial.print(" = ");
+  Serial.println(micros);
 }
 
-void coxaFwd(int id) {
-  // leg fwd
-  pwm.writeMicroseconds(id, 1000); // shoulder
+/*
+  - microservos service /timer interrupt function/50Hz
+  - when set site expected,this function move the end point to it in a straight line
+  - temp_speed[4][3] should be set before set expect site,it make sure the end point
+   move in a straight line,and decide move speed.
+   ---------------------------------------------------------------------------*/
+#define E_DELTA 0.01
+void servo_service(void)
+{
+  sei();
+  static float alpha, beta, gamma;
+
+  for (int i = 0; i < 4; i++)
+  {
+    for (int j = 0; j < 3; j++)
+    {
+      if (abs(site_now[i][j] - site_expect[i][j]) < (abs(temp_speed[i][j])+E_DELTA))
+        site_now[i][j] = site_expect[i][j];
+      else
+        site_now[i][j] += temp_speed[i][j];
+    }
+
+    cartesian_to_polar(alpha, beta, gamma, site_now[i][0], site_now[i][1], site_now[i][2]);
+    polar_to_servo(i, alpha, beta, gamma);
+  }
+
+  rest_counter++;
 }
 
-void legDown(int femur, int tibia) {
-  // leg down
-  pwm.writeMicroseconds(femur, 900); // tip
-  pwm.writeMicroseconds(tibia, 2000); // femur
+/*
+  - set one of end points' expect site
+  - this founction will set temp_speed[4][3] at same time
+  - non - blocking function
+   ---------------------------------------------------------------------------*/
+void set_site(int leg, float x, float y, float z)
+{
+  float length_x = 0, length_y = 0, length_z = 0;
+
+  if (x != KEEP)
+    length_x = x - site_now[leg][0];
+  if (y != KEEP)
+    length_y = y - site_now[leg][1];
+  if (z != KEEP)
+    length_z = z - site_now[leg][2];
+
+  float length = sqrt(pow(length_x, 2) + pow(length_y, 2) + pow(length_z, 2));
+
+  temp_speed[leg][0] = length_x / length * move_speed * speed_multiple;
+  temp_speed[leg][1] = length_y / length * move_speed * speed_multiple;
+  temp_speed[leg][2] = length_z / length * move_speed * speed_multiple;
+
+  if (x != KEEP)
+    site_expect[leg][0] = x;
+  if (y != KEEP)
+    site_expect[leg][1] = y;
+  if (z != KEEP)
+    site_expect[leg][2] = z;
 }
 
-void legUp(uint8_t femur, uint8_t tibia) {
-  // leg up
-  pwm.writeMicroseconds(femur, 2000); // tibia
-  pwm.writeMicroseconds(tibia, 2000); // femur
+/*
+  - wait one of end points move to expect site
+  - blocking function
+   ---------------------------------------------------------------------------*/
+void wait_reach(int leg)
+{
+  while (1)
+    if (site_now[leg][0] == site_expect[leg][0])
+      if (site_now[leg][1] == site_expect[leg][1])
+        if (site_now[leg][2] == site_expect[leg][2])
+          break;
 }
 
-void legNeutral(uint8_t femur, uint8_t tibia) {
-  // leg neutral
-  pwm.writeMicroseconds(femur, 1700);
-  pwm.writeMicroseconds(tibia, 1600);
+/*
+  - wait all of end points move to expect site
+  - blocking function
+   ---------------------------------------------------------------------------*/
+void wait_all_reach(void)
+{
+  for (int i = 0; i < 4; i++)
+    wait_reach(i);
+}
+
+/*
+  - trans site from cartesian to polar
+  - mathematical model 2/2
+   ---------------------------------------------------------------------------*/
+void cartesian_to_polar(volatile float &alpha, volatile float &beta, volatile float &gamma, volatile float x, volatile float y, volatile float z)
+{
+  //calculate w-z degree
+  float v, w;
+  w = (x >= 0 ? 1 : -1) * (sqrt(pow(x, 2) + pow(y, 2)));
+  v = w - length_c;
+  alpha = atan2(z, v) + acos((pow(length_a, 2) - pow(length_b, 2) + pow(v, 2) + pow(z, 2)) / 2 / length_a / sqrt(pow(v, 2) + pow(z, 2)));
+  beta = acos((pow(length_a, 2) + pow(length_b, 2) - pow(v, 2) - pow(z, 2)) / 2 / length_a / length_b);
+  //calculate x-y-z degree
+  gamma = (w >= 0) ? atan2(y, x) : atan2(-y, -x);
+  //trans degree pi->180
+  alpha = alpha / pi * 180;
+  beta = beta / pi * 180;
+  gamma = gamma / pi * 180;
+}
+
+/*
+  - trans site from polar to microservos
+  - mathematical model map to fact
+  - the errors saved in eeprom will be add
+   ---------------------------------------------------------------------------*/
+void polar_to_servo(int leg, float alpha, float beta, float gamma)
+{
+  if (leg == 0)
+  {
+    alpha = 90 - alpha;
+    beta = beta;
+    gamma += 90;
+  }
+  else if (leg == 1)
+  {
+    alpha += 90;
+    beta = 180 - beta;
+    gamma = 90 - gamma;
+  }
+  else if (leg == 2)
+  {
+    alpha += 90;
+    beta = 180 - beta;
+    gamma = 90 - gamma;
+  }
+  else if (leg == 3)
+  {
+    alpha = 90 - alpha;
+    beta = beta;
+    gamma += 90;
+  }
+
+  servo_write(leg, 0, alpha);
+  servo_write(leg, 1, beta);
+  servo_write(leg, 2, gamma);
 }
